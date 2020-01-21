38988d53c83344bfcd5822d59e48fca49bb1ab16
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-01-21 18:49:56 -0300

    Add the files 'repos.txt' and 'update_to_hash.py'

---
+++ update_to_hash/update_to_hash.py
@@ -0,0 +1,6 @@
+## @package update_to_hash.py
+#  Automatic checkout using a given txt file with repositories.
+#
+#  With this code it is possible to checkout for specific projects
+#     using the tag, hash or branch informed in the file repos.txt.
+
