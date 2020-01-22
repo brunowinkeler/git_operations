8bb2659fc3aaacfa6dc804e076aaf05aef1c90f5
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-01-22 20:48:05 -0300

    Add the file "generate_log.py"

---
+++ generate_log/generate_log.py
@@ -0,0 +1,7 @@
+## @package generate_log.py
+#  Generate a personalized log using a given txt file with repositories.
+#
+#  With this code it is possible to generate a log with projects' hashs 
+#     informed in the file repos.txt.
+#
+#  An output file called "YYYY_MM_DD - LOG.txt" is generated.
