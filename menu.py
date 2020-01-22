b6c3ee1ee6fadeb9d54da0c9dbf1149d8d7cd4f5
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-01-22 20:55:58 -0300

    Add the file 'menu.py'

---
+++ menu.py
@@ -0,0 +1,5 @@
+## @package menu.py
+#  Create a personalized menu to call other features routines
+#
+#  With this code it is possible to integrate all features developed
+#    in an only one menu.
