87f352250078cec755a89784b53373b597a037e9
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-01-22 21:37:23 -0300

    Add the options to the menu and edit logo

--- menu.py
+++ menu.py
@@ -4,6 +4,11 @@
 #  With this code it is possible to integrate all features developed
 #    in an only one menu.
 
+import os
+
+os.system('cls')
+
+# Logo ##############################################################
 
 print('_'*95)
 
@@ -13,6 +18,21 @@ for line_ in file_:
     print(line_.replace('\n',''))
 
 print('_'*95)
+
+
+# MENU ###################################################################
+
+print("\nWhich of the following options would you like to run?\n".upper())
+
+print('1 - Update to Hash')
+print('2 - Generate Log')
+print('3 - Compile All')
+
+ans_ = input('\nType your answer: '.upper())
+
+print('\n')
+
+
 
 
 
