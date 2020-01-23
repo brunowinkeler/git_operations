62fc07cef56d7ba9004c5b8317d827b7a33c7ad7
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-01-23 13:36:50 -0300

    Add interactivity to the menu.

--- menu.py
+++ menu.py
@@ -18,22 +18,36 @@ for line_ in file_:
     print(line_.replace('\n',''))
 
 print('_'*95)
+print('\n')
 
 
 # MENU ###################################################################
+def options():
+    print("Which of the following options would you like to run?\n".upper())
 
-print("\nWhich of the following options would you like to run?\n".upper())
+    print('1 - Update to Hash')
+    print('2 - Generate Log [NOT AVAILABLE]')
+    print('3 - Compile All [NOT AVAILABLE]')
+    print('x - Exit')
 
-print('1 - Update to Hash')
-print('2 - Generate Log [NOT AVAILABLE]')
-print('3 - Compile All [NOT AVAILABLE]')
+    ans_ = input('\nType your answer: '.upper())
 
-ans_ = input('\nType your answer: '.upper())
+    return ans_
 
-print('\n')
+opt_ = options()
 
-if(ans_ == '1'):
-    os.system('python ./update_to_hash/update_to_hash.py')
+while( True ):
+    if(opt_ == '1'):
+        os.system('python ./update_to_hash/update_to_hash.py')
+    elif( opt_ == 'x'):
+        print('BYE!')
+        break
+    else:
+        print('xxxxxxxxxxxxxxxxx OPTION NOT AVAILABLE! xxxxxxxxxxxxxxxxx \n')
+
+    opt_ = options()
+        
+    
 
 
 
