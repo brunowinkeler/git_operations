71ce000a823607f2956e03e1d91d7e73c5e02d47
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-01-22 21:10:03 -0300

    Add logo in the menu

--- menu.py
+++ menu.py
@@ -3,3 +3,16 @@
 #
 #  With this code it is possible to integrate all features developed
 #    in an only one menu.
+
+
+print('_'*95)
+
+file_ = open('utils/logo.txt', 'r')
+
+for line_ in file_:
+    print(line_.replace('\n',''))
+
+print('_'*95)
+
+
+
